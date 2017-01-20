d692cfff6b2b6512d9e60518d6d5d76ddf654add
fmarocchi92 <fabio.marocchi@mail.polimi.it>
2017-01-20 20:55:58 +0100

    Test commit

---
+++ Assets/Scripts/Brush.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brush : MonoBehaviour {
+	public GameObject paint;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool pressed = Input.GetMouseButton (0);
+		if (pressed) {
+			GameObject newPaint = GameObject.Instantiate (paint);
+			newPaint.transform.position = Input.mousePosition;
+		}
+	}
+}
