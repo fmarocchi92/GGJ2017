a0a2ada62c5ba6c10b0327cfdce77f7150de0e70
StefanoDesiati <desiati.stefano@gmail.com>
2017-01-21 01:31:23 +0100

    TrailWithObjects

--- Assets/Scripts/WaivePaint.cs
+++ Assets/Scripts/WaivePaint.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WaivePaint : MonoBehaviour {
-	public float speed;
+	public float speed = 0;
+	public GameObject paint;
+	public float accel = 0;
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+
+
 		transform.position = new Vector3 (transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+		if (Input.GetKey (KeyCode.W)) {
+			accel = accel + 0.5f;
+			transform.position = new Vector3 (transform.position.x, transform.position.y + accel * Time.deltaTime, transform.position.z);
+		} else {
+			accel = accel - 0.5f;
+			transform.position = new Vector3 (transform.position.x, transform.position.y + accel * Time.deltaTime, transform.position.z);
+			}
+
+		GameObject newPaint = GameObject.Instantiate (paint);
+		newPaint.transform.position = transform.position;
 	}
 }
