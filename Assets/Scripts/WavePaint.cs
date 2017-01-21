3233a9ef43e68dd5fb4a02142b7a6604505b91f3
fmarocchi92 <fabio.marocchi@mail.polimi.it>
2017-01-21 01:28:26 +0100

    Trail

--- Assets/Scripts/WavePaint.cs
+++ Assets/Scripts/WavePaint.cs
@@ -6,8 +6,11 @@ public class WavePaint : MonoBehaviour {
 	public float hSpeed;
 	public float vSpeed;
 	private float deltaY = 0;
+	public float pitchRange = 12;
+	private AudioSource audioSource;
 	// Use this for initialization
 	void Start () {
+		audioSource = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
@@ -15,12 +18,18 @@ public class WavePaint : MonoBehaviour {
 	//INPUT
 		bool pressed = Input.GetKey(KeyCode.Space);
 	//MOVEMENT CONTROL
+		if (transform.position.y +deltaY * Time.deltaTime > pitchRange / 2 || transform.position.y +deltaY * Time.deltaTime < -pitchRange / 2)
+			deltaY = 0;
 		if (pressed) {
 			deltaY += vSpeed;
 		} else {
 			deltaY -= vSpeed;
 		}
-		transform.position = new Vector3 (transform.position.x + hSpeed * Time.deltaTime, transform.position.y +deltaY * Time.deltaTime, transform.position.z);
 
+		transform.position = new Vector3 (transform.position.x + hSpeed * Time.deltaTime, transform.position.y +deltaY * Time.deltaTime, transform.position.z);
+		float tempPitch =  transform.position.y / pitchRange;
+		tempPitch=Mathf.Clamp01 (tempPitch+0.5f);
+
+		audioSource.pitch = tempPitch==0?0.1f:(tempPitch * 3);
 	}
 }
