c825156b845a2537dd1aab1c7075db63ad147d24
StefanoDesiati <desiati.stefano@gmail.com>
2017-01-21 07:45:17 +0100

    Robot Movement

---
+++ Assets/Scripts/RoboMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoboMovement : MonoBehaviour {
+	public static int x = 0;
+	public static Vector3[] vettore;
+	public float offset = 131.7f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (WaivePaint.i > 300){
+			transform.position = new Vector3 (vettore [x].x, vettore [x].y - offset, vettore [x].z);
+
+			//transform.LookAt (new Vector3(vettore [x].x, vettore [x].y - offset , vettore [x].z));
+			
+			WaivePaint.i++;
+			x++;
+
+
+
+	}
+
+
+
+}
+	public static void startMovement(){
+		vettore = WaivePaint.list.ToArray(typeof(Vector3)) as Vector3[];
+
+	}
+}
--- Assets/Scripts/WaivePaint.cs
+++ Assets/Scripts/WaivePaint.cs
@@ -6,25 +6,38 @@ public class WaivePaint : MonoBehaviour {
 	public float speed = 0;
 	public GameObject paint;
 	public float accel = 0;
+	public static int i = 0;
+	public static ArrayList list = new ArrayList();
+	public float acceladd = 0.3f;
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (i < 300) {
+
+
 		
 
 
-		transform.position = new Vector3 (transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
-		if (Input.GetKey (KeyCode.W)) {
-			accel = accel + 0.5f;
-			transform.position = new Vector3 (transform.position.x, transform.position.y + accel * Time.deltaTime, transform.position.z);
-		} else {
-			accel = accel - 0.5f;
-			transform.position = new Vector3 (transform.position.x, transform.position.y + accel * Time.deltaTime, transform.position.z);
+			transform.position = new Vector3 (transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+			if (Input.GetKey (KeyCode.W)) {
+				accel = accel + acceladd;
+				transform.position = new Vector3 (transform.position.x, transform.position.y + accel * Time.deltaTime, transform.position.z);
+			} else {
+				accel = accel - acceladd;
+				transform.position = new Vector3 (transform.position.x, transform.position.y + accel * Time.deltaTime, transform.position.z);
 			}
 
-		GameObject newPaint = GameObject.Instantiate (paint);
-		newPaint.transform.position = transform.position;
+			GameObject newPaint = GameObject.Instantiate (paint);
+			newPaint.transform.position = transform.position;
+			list.Add (transform.position);
+			i++;
+		} else if (i == 300) {
+			RoboMovement.startMovement ();
+			i++;
+		}
+
 	}
 }
