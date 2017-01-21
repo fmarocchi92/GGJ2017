3ca79cb3fb37b33e8e67c38bcf7fc449e6e2ff0e
fmarocchi92 <fabio.marocchi@mail.polimi.it>
2017-01-21 04:38:53 +0100

    Game Manager initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public GameObject[] waveHeads;
+	public GameObject[] spotLights;
+	public GameObject[] checkPoints;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < waveHeads.Length; i++) {
+//			waveHeads [i].GetComponent<WaveController> ().start ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void waveHit(int playerIndex){
+		
+		GameObject newSpotlight = Light.Instantiate (spotLights[playerIndex]);
+		newSpotlight.transform.position = new Vector3(waveHeads[playerIndex].transform.position.x, waveHeads[playerIndex].transform.position.y,-5);
+	}
+}
