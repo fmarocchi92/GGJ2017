0314f09d185a71952d42e693c1bd440e06106e1f
fmarocchi92 <fabio.marocchi@mail.polimi.it>
2017-01-22 02:25:41 +0100

    Temp Block Lights  fixed

---
+++ Assets/Scripts/BlockLightController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockLightController : MonoBehaviour {
+	GameObject[] players;
+	Material material;
+	Color defaultColor;
+	// Use this for initialization
+	void Start () {
+		players=GameManager.getPlayers ();
+		print (players);
+		material = GetComponent<Renderer> ().material;
+		defaultColor = material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool lightUp = false;
+		if (players == null) {
+			players = GameManager.getPlayers ();
+		}
+		for (int i = 0; i < players.Length; i++) {
+			if (players [i].transform.position.z > transform.position.z) {
+				//CHANGE SHADER PARAMETER
+				lightUp = true;
+			} 
+		}
+		if (lightUp) {
+			material.color = Color.cyan;
+		} else {
+			material.color = defaultColor;
+		}
+	}
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,21 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour{
 	public GameObject[] players;
 	public GameObject[] waveHeads;
 	public GameObject[] spotLights;
 	public GameObject[] checkPoints;
+	public static GameObject[] playerList;
 
 	// Use this for initialization
 	void Start () {
 //		for (int i = 0; i < players.Length; i++) {
 //			waveHeads [i].GetComponent<WaveController> ().start ();
 //		}
-		print("3,2,1");
 		for (int i = 0; i < players.Length; i++) {
 			waveHeads [i].GetComponent<WaveController> ().ready = true;
 		}
+		playerList = players;
 	}
 	
 	// Update is called once per frame
@@ -25,9 +26,12 @@ public class GameManager : MonoBehaviour {
 	}
 
 	public void waveHit(int playerIndex){
-		print ("playerHit");
 		GameObject newSpotlight = Light.Instantiate (spotLights[playerIndex]);
 		newSpotlight.transform.position = new Vector3(players[playerIndex].transform.position.x +5, players[playerIndex].transform.position.y,players[playerIndex].transform.position.z);
 		newSpotlight.transform.rotation = Quaternion.Euler (0, -90, 0);
+	}
+
+	public static GameObject[] getPlayers(){
+		return playerList;
 	}
 }
